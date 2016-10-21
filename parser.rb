68f61d4b47dbef15f2d6410cf8679d17ba19b54b
PissyPoops <jandrewbrown94@gmail.com>
2016-10-21 03:01:20 -0500

    Update parser.rb

--- parser.rb
+++ parser.rb
@@ -6,14 +6,16 @@ file_frequency = Hash.new
 statuses = Hash.new
 daily_requests= Hash.new
 monthly_requests = Hash.new
+monthly_requests_count = Hash.new
 unix_time = []
 failure_array = []
+_300s = []
+_400s = []
 
 remote_file = 'http://s3.amazonaws.com/tcmg412-fall2016/http_access_log'
-local_file = 'log_file.txt'
+log_file = 'log_file.txt'
 test_file = 'log_file_shortened.txt'
-pad = '     '
-extra_pad = '         '
+splitter_path = 'logs_by_month/'
 
 def winsize
   require 'io/console'
@@ -31,6 +33,7 @@ def smallest_hash(hash)
   min = hash.values.min
   Hash[hash.select { |k,v| v == min }]
 end
+
 def incrementor(h,a)
   h[a] = (
     if h[a] then
@@ -42,28 +45,30 @@ end
 def question_formatter(q_num)
   rows, columns = winsize
   puts "\n\nQuestion #{q_num}".center(columns)
-  puts "".center((columns), "~")+"\n\n"
+  puts "".center((columns), "~")+"\n"
 end
 
-#Ask user if they'd like to download the file/confirms execution of program
-puts 'Would you like to retrieve the file for parsing? (Y/N)'.center(columns)
+#Ask user if they'd like to execute the program
+
+puts "\n\n\nBegin parsing? (Y/N)".center(columns)
 response = gets.chomp.upcase
 if response == "Y" 
   puts "     Downloading log file from " + remote_file + ".\n"
   download = open(remote_file)
-  IO.copy_stream(download, local_file)
+  IO.copy_stream(download, log_file)
   puts "     The file " + remote_file + " has been downloaded and saved to your directory.\n"
   else
-    puts "     Then why are you here?\n"
+    puts "     That's fine....\n"
 end
 #Counts the total number of requests (lines) in the file. 
-File.foreach(test_file) {}
+File.foreach(log_file) {}
 total_requests = $.
 #Loops through saved file
-File.foreach(test_file) do |x|
-	delimited = /.*\[(.*) \-[0-9]{4}\] \"([A-Z]+) (.+?)( HTTP.*\"|\") ([2-5]0[0-9]) .*/.match(x)
+
+File.foreach(log_file) do |data|
+	delimited = /.*\[(.*) \-[0-9]{4}\] \"([A-Z]+) (.+?)( HTTP.*\"|\") ([2-5]0[0-9]) .*/.match(data)
   if !delimited
-    failure_array.push(x)
+    failure_array.push(data)
     next
   end
 	# Collect groups from #{delimited} and organize the into arrays
@@ -74,19 +79,23 @@ File.foreach(test_file) do |x|
 	request = delimited[2]
 	file_request = delimited[3]
 	status = delimited[5]
-  #
-  incrementor(monthly_requests, y_m)
+
+  unless monthly_requests[y_m]
+    monthly_requests[y_m] = [] end
+  monthly_requests[y_m].push(data)
+  
+  incrementor(monthly_requests_count, y_m)
   incrementor(file_frequency, file_request)
   incrementor(statuses, status)
 
 end
 
-                          #########OUTPUTS########
+#########OUTPUTS########
 question_formatter(1)
 puts "     There were a total of #{total_requests} requests in the log file."
 
 question_formatter(2)
-monthly_requests_a = monthly_requests.to_a
+monthly_requests_a = monthly_requests_count.to_a
 puts "     Year-Month        Number of Requests"
 puts "     ------------------------------------"
 monthly_requests_a.each do |date, requests|
@@ -95,31 +104,24 @@ end
 
 question_formatter(3)
 unsuccessful_requests = (((statuses['404'].to_f+ statuses['403'].to_f)/total_requests)*100)
-puts unsuccessful_requests
+puts "     #{unsuccessful_requests}% of all requests were unsuccessful (4xx status code)."
 
 question_formatter(4)
-percentage_300s = ((_300s.to_f/total_requests.to_f)*100)
-puts "     A total of #{_300s} requests, or #{percentage_300s}%, were redirected."
-
-def request_types_sum(hash)
-  hash.each do |status, frequency|
-    if status[0] == '3' then _300s += 1 end
-    if status[0] == '4' then _400s += 1 end
-  end
-end
-
-puts request_types_sum(statuses)
+redirected_requests = (((statuses['302'].to_f+ statuses['304'].to_f)/total_requests)*100)
+puts "     #{redirected_requests}% of all requests were redirected (3xx status code)."
 
 question_formatter(5)
-puts largest_hash(file_frequency)
-puts largest_hash(file_frequency).length
+file_frequency_most = largest_hash(file_frequency).to_a.sort
+file_frequency_most.each do |file, requests|
+  print "     \"#{file}\" was the most wanted file with #{requests} total requests."
+end
 
 question_formatter(6)
 file_frequency_least = smallest_hash(file_frequency).to_a.sort
 
-if file_frequency_least.length > 100
-  puts "     There are over #{file_frequency_least.length} individual files that have been requested only once."
-  puts "     Would you like to view all of these individual files?" + "\n\n"
+if file_frequency_least.length > 1
+  puts "     There are over #{file_frequency_least.length} individual files that are tied for least amount of requests.".center(columns)
+  puts "     Would you like to view all of these individual files? (Y/N)\n".center(columns)
   file_least_response = gets.chomp.upcase
   if file_least_response == 'Y'
     file_frequency_least.each do |file, requests|
@@ -127,11 +129,44 @@ if file_frequency_least.length > 100
       puts
     end
   else
-    puts "          OK. Not printing #{file_frequency_least.length} files." + "\n\n"
+    puts "     OK. Not printing #{file_frequency_least.length} files." + "\n"
   end
   else
   file_frequency_least.each do |file, requests|
-    print "#{file} "
-    puts
+    print "#{file} \n"
   end
 end
+
+#######FILE-SPLITTER#######
+Dir.mkdir(splitter_path) unless File.exists?(splitter_path)
+monthly_requests.each do |k,v|
+	divided_name = splitter_path + k + '.log'
+	File.open(divided_name, "w+") do |write|
+		write.puts(v)
+	end
+end
+puts "Logs catorgorized by year and month and saved at #{splitter_path}.\n\n"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
